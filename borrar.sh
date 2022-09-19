703078869e9d691e20b8220cdca545a45b47a1bb
Lautaro Barba <lau_mb94@hotmail.com>
2022-09-19 10:52:01 -0300

    Se arreglo bug al seleccionar nombres de archivos

--- borrar.sh
+++ borrar.sh
@@ -21,7 +21,7 @@ LOG_DIR="./borrar_log.log"  # Archivo donde se guardarán los logs
 echo "== Script =="
 
 CANT_ARCHIVOS=$(( $(ls ${DIR_BACKUPS} -l | wc -l) - 1 ))
-echo "Cantidad maxima permitida: ${CANT_MAX}. Hay ${CANT_ARCHIVOS} archivos."
+echo "Cantidad maxima permitida: ${CANT_MAX}. Hay ${CANT_ARCHIVOS} archivos." >> ${LOG_DIR}
 
 echo "$(date) Revisando..." >> ${LOG_DIR}
 
@@ -31,9 +31,9 @@ then
     echo "$(date) Borrando archivos..." >> ${LOG_DIR}
     echo "Se borraran los siguientes archivos: "
     echo "$(date) Se borraran los siguientes archivos: " >> ${LOG_DIR}
-    ls ${DIR_BACKUPS} -l | sort | head -n $(( ${CANT_ARCHIVOS} - ${CANT_MAX} )) | cut -d ' ' -f 9 | xargs printf -- "   -> ${DIR_BACKUPS}/%s \n"
-    ls ${DIR_BACKUPS} -l | sort | head -n $(( ${CANT_ARCHIVOS} - ${CANT_MAX} )) | cut -d ' ' -f 9 | xargs printf -- "$(date)    -> ${DIR_BACKUPS}/%s \n" >> ${LOG_DIR}
-    ls ${DIR_BACKUPS} -l | sort | head -n $(( ${CANT_ARCHIVOS} - ${CANT_MAX} )) | cut -d ' ' -f 9 | xargs printf -- "${DIR_BACKUPS}/%s " | xargs rm
+    ls ${DIR_BACKUPS} -l | sort | head -n $(( ${CANT_ARCHIVOS} - ${CANT_MAX} )) | awk '{print $9}' | xargs printf -- "   -> ${DIR_BACKUPS}/%s \n"
+    ls ${DIR_BACKUPS} -l | sort | head -n $(( ${CANT_ARCHIVOS} - ${CANT_MAX} )) | awk '{print $9}' | xargs printf -- "$(date)    -> ${DIR_BACKUPS}/%s \n" >> ${LOG_DIR}
+    ls ${DIR_BACKUPS} -l | sort | head -n $(( ${CANT_ARCHIVOS} - ${CANT_MAX} )) | awk '{print $9}' | xargs printf -- "${DIR_BACKUPS}/%s " | xargs rm
 else
     echo "No hace falta borrar nada."
     echo "$(date) No hace falta borrar nada." >> ${LOG_DIR}
